e5780d47aed07f97a518d5828baccf26240b9d4a
Alan Gomez <gomezapazalansamuel@gmail.com>
2025-10-27 17:37:47 -0600

    Se agrega la Alu version 2, no he realizado algo

---
+++ ALU Control y Banco de Registros (RegisterFile)/ALU/ALU.v
@@ -0,0 +1,13 @@
+module ALUControl (
+    input [1:0] ALUOp,
+    input [5:0] funct,
+    output reg [3:0] alu_control
+);
+always @(*) begin
+    case (param)
+        : 
+        default: 
+    endcase
+end
+    
+endmodule
